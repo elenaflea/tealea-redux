ecbfd1757ce37f105a2be6c03ec6248bd14b69f1
Elena GRONDIN <88861549+elenaflea@users.noreply.github.com>
2022-04-11 22:31:58 +0200

    Update Filter.js

--- src/Components/Filter/Filter.js
+++ src/Components/Filter/Filter.js
@@ -10,13 +10,13 @@ const Filter = () => {
 
     return ( 
         <div className='column'>
-            <button className='change noir' onClick={ ()=> dispatch(filterCategory("tout")) } > Tout </button> 
-            <button className='change vert' onClick={ ()=> dispatch(filterCategory("vert")) }  > Thés verts </button>
-            <button className='change noir' onClick={ ()=> dispatch(filterCategory("noir")) } > Thés noirs </button>
-            <button className='change noel' onClick={ ()=> dispatch(filterCategory("noel")) } > Thés de Noël </button>
-            <button className='change rouge' onClick={ ()=> dispatch(filterCategory("rouge")) } > Rooibos - Thés Rouges </button>
-            <button className='change blanc' onClick={ ()=> dispatch(filterCategory("blanc")) } > Thés Blancs </button> 
-            <button className='change detox' onClick={ ()=> dispatch(filterCategory("detox")) } > Bien-être - Détox </button> 
+            <button tabindex="0" className='change noir' onClick={ ()=> dispatch(filterCategory("tout")) } > Tout </button> 
+            <button tabindex="0" className='change vert' onClick={ ()=> dispatch(filterCategory("vert")) }  > Thés verts </button>
+            <button tabindex="0" tabindex="0" className='change noir' onClick={ ()=> dispatch(filterCategory("noir")) } > Thés noirs </button>
+            <button tabindex="0" className='change noel' onClick={ ()=> dispatch(filterCategory("noel")) } > Thés de Noël </button>
+            <button tabindex="0" className='change rouge' onClick={ ()=> dispatch(filterCategory("rouge")) } > Rooibos - Thés Rouges </button>
+            <button tabindex="0" className='change blanc' onClick={ ()=> dispatch(filterCategory("blanc")) } > Thés Blancs </button> 
+            <button tabindex="0" className='change detox' onClick={ ()=> dispatch(filterCategory("detox")) } > Bien-être - Détox </button> 
             
         </div>
     )
