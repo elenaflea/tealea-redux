668439d11fd7e9ac0636c03e7273ebd146948a5d
Elena GRONDIN <88861549+elenaflea@users.noreply.github.com>
2022-04-11 22:38:33 +0200

    Update Filter.js

--- src/Components/Filter/Filter.js
+++ src/Components/Filter/Filter.js
@@ -12,7 +12,7 @@ const Filter = () => {
         <div className='column'>
             <button tabindex="0" className='change noir' onClick={ ()=> dispatch(filterCategory("tout")) } > Tout </button> 
             <button tabindex="1" className='change vert' onClick={ ()=> dispatch(filterCategory("vert")) }  > Thés verts </button>
-            <button tabindex="2" tabindex="0" className='change noir' onClick={ ()=> dispatch(filterCategory("noir")) } > Thés noirs </button>
+            <button tabindex="2" className='change noir' onClick={ ()=> dispatch(filterCategory("noir")) } > Thés noirs </button>
             <button tabindex="3" className='change noel' onClick={ ()=> dispatch(filterCategory("noel")) } > Thés de Noël </button>
             <button tabindex="4" className='change rouge' onClick={ ()=> dispatch(filterCategory("rouge")) } > Rooibos - Thés Rouges </button>
             <button tabindex="5" className='change blanc' onClick={ ()=> dispatch(filterCategory("blanc")) } > Thés Blancs </button> 
