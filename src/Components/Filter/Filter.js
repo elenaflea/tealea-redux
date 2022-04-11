2d34e58590cad5b45f5ecbcc369273a1816f71e9
Elena GRONDIN <88861549+elenaflea@users.noreply.github.com>
2022-04-11 22:36:22 +0200

    Update Filter.js

--- src/Components/Filter/Filter.js
+++ src/Components/Filter/Filter.js
@@ -11,12 +11,12 @@ const Filter = () => {
     return ( 
         <div className='column'>
             <button tabindex="0" className='change noir' onClick={ ()=> dispatch(filterCategory("tout")) } > Tout </button> 
-            <button tabindex="0" className='change vert' onClick={ ()=> dispatch(filterCategory("vert")) }  > Thés verts </button>
-            <button tabindex="0" tabindex="0" className='change noir' onClick={ ()=> dispatch(filterCategory("noir")) } > Thés noirs </button>
-            <button tabindex="0" className='change noel' onClick={ ()=> dispatch(filterCategory("noel")) } > Thés de Noël </button>
-            <button tabindex="0" className='change rouge' onClick={ ()=> dispatch(filterCategory("rouge")) } > Rooibos - Thés Rouges </button>
-            <button tabindex="0" className='change blanc' onClick={ ()=> dispatch(filterCategory("blanc")) } > Thés Blancs </button> 
-            <button tabindex="0" className='change detox' onClick={ ()=> dispatch(filterCategory("detox")) } > Bien-être - Détox </button> 
+            <button tabindex="1" className='change vert' onClick={ ()=> dispatch(filterCategory("vert")) }  > Thés verts </button>
+            <button tabindex="2" tabindex="0" className='change noir' onClick={ ()=> dispatch(filterCategory("noir")) } > Thés noirs </button>
+            <button tabindex="3" className='change noel' onClick={ ()=> dispatch(filterCategory("noel")) } > Thés de Noël </button>
+            <button tabindex="4" className='change rouge' onClick={ ()=> dispatch(filterCategory("rouge")) } > Rooibos - Thés Rouges </button>
+            <button tabindex="5" className='change blanc' onClick={ ()=> dispatch(filterCategory("blanc")) } > Thés Blancs </button> 
+            <button tabindex="6" className='change detox' onClick={ ()=> dispatch(filterCategory("detox")) } > Bien-être - Détox </button> 
             
         </div>
     )
