0ac2b8f3962a3e3cfd1fc8fc84e22f8cd9318a8b
Elena GRONDIN <88861549+elenaflea@users.noreply.github.com>
2022-05-22 16:33:05 +0200

    Add files via upload

---
+++ src/Components/Welcome.js
@@ -0,0 +1,16 @@
+const Welcome =()=>{
+
+   
+    return (
+        <div className="welcomePage App">
+<p className="teteWhite">Mélanges de thé & tisane </p>
+<p className='welcomePar ' >pour vous guider d’une longue journée, directement dans le pays des rêves...</p>
+<br/> <br/>
+<a className='btnSendTwo' href= "#down" >
+        voir les produits
+      </a>
+        </div>
+    )
+}
+
+export default Welcome;
